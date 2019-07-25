bf53242b32722aa0b9b6afb1af8da505a2c60e47
wengli571 <wengli571@jk.cn>
2019-07-25 16:02:14 +0800

    update

--- PythonDemo/src/testpython/nice.py
+++ PythonDemo/src/testpython/nice.py
@@ -6,7 +6,8 @@ import smtplib
 from email.mime.text import MIMEText
 import datetime
 from nice_airtest import nice_airtest
-
+import urllib3
+urllib3.disable_warnings()
 
 def sendqqEmail(subject,content,msg_to='315118616@qq.com'):    #收件人邮箱
     msg_from='1056396153@qq.com'                               #发送方邮箱
@@ -59,30 +60,29 @@ def output_value(jsons, key):
     if key_value!='':
         print(str(key)+" = "+str(key_value))
 
-class nice():
-    def getPrice(self,url,data):
-        h = {
-            "Cache-Control": "no-cache",
-            "Host": "api.oneniceapp.com",
-            "Content-Type": "application/json; charset=utf-8",
-            "Content-Length": "123",
-            "Connection": "Keep-Alive",
-            "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "zh-Hans-CN;q=1, ja-JP;q=0.9",
-            "User-Agent": "KKShopping/5.4.2 (iPhone 8; iOS 12.3.1; Scale/2.00)"
-        }
-        try:
-            r = requests.post(url,headers=h,data=data,verify=False,timeout=2)
-            # response = r.text
-            # print (response)
-            return r
-        except:
-            return False
+def getPrice(url,data):
+    h = {
+        "Cache-Control": "no-cache",
+        "Host": "api.oneniceapp.com",
+        "Content-Type": "application/json; charset=utf-8",
+        "Content-Length": "123",
+        "Connection": "Keep-Alive",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-Hans-CN;q=1, ja-JP;q=0.9",
+        "User-Agent": "KKShopping/5.4.2 (iPhone 8; iOS 12.3.1; Scale/2.00)"
+    }
+    try:
+        r = requests.post(url,headers=h,data=data,verify=False,timeout=2)
+        # response = r.text
+        # print (response)
+        return r
+    except:
+        return False
 
-    def calculatePrice(self,response,name,chajia):
+def calculatePrice(r,name,chajia):
         try:
-            if(response.status_code==200):
+            if(r.status_code==200):
                 response=r.text
                 # print (response)
                 # print (type(response))
@@ -90,17 +90,30 @@ class nice():
                 # print (d)
                 xianhuo={}
                 shangou={}
-                for i in d["data"]["list"]:
-                    if(i["stock_id"]=="1"):
-                        size=str(i["size"])
-                        price=str(i["price"])
-                        xianhuo[size]=price
-                    elif(i["stock_id"]=="128"):
-                        size=str(i["size"])
-                        price=str(i["price"])
-                        shangou[size]=price
-                # print "现货"+str(xianhuo)
-                # print "闪购"+str(shangou)
+                try:
+                    for i in d["data"]["list"]:
+                        if(i["stock_id"]=="1"):
+                            size=str(i["size"])
+                            price=str(i["price"])
+                            xianhuo[size]=price
+                        elif(i["stock_id"]=="128"):
+                            size=str(i["size"])
+                            price=str(i["price"])
+                            shangou[size]=price
+                except:
+                    for i in d["data"]["tab_list"]:
+                        for x in i["list"]:
+                            # print (x)
+                            if (x["stock_id"] == "1"):
+                                size = str(x["size"])
+                                price = str(x["price"])
+                                xianhuo[size] = price
+                            elif (x["stock_id"] == "128"):
+                                size = str(x["size"])
+                                price = str(x["price"])
+                                shangou[size] = price
+                # print ("现货"+str(xianhuo))
+                # print ("闪购"+str(shangou))
                 print(".", end='',flush=True)
                 for i in shangou:
                     # print i
@@ -131,11 +144,18 @@ class nice():
         except Exception as e:
             print("F", end='',flush=True)
             # print("F"),
-        time.sleep(0.5)
+
+def bot(url,data,name,chajia,sleep=5):
+    while (True):
+        X=judgeTime("07:20", "23:59")
+        if(X):
+            while(judgeTime("07:20", "23:59")):
+                r=getPrice(url,data)
+                calculatePrice(r,name,chajia)
+                time.sleep(sleep)
 
 if __name__ == "__main__":
     url="http://api.oneniceapp.com/Sneakerpurchase/priceInfos?a_x=0.006302&a_y=-0.013901&a_z=-0.997620&abroad=no&amap_latitude=31.251428&amap_longitude=121.572066&appv=5.4.2.20&ch=AppStore_5.4.2.20&did=be09b9b291195c16a08ba082a1a4f05b&dn=Only%20iPhone&dt=iPhone10%2C1&g_x=-0.013020&g_y=-0.003731&g_z=-0.012055&geoacc=10&im=8B828531-62AC-4D20-A4EC-1B06DC4DCC2B&la=cn&latitude=31.253600&lm=weixin&longitude=121.567834&lp=-1.000000&n_bssid=&n_dns=fe80%3A%3A1&n_ssid=&net=0-0-wifi&osn=iOS&osv=12.3.1&seid=62ba1882f758643933503deb6c1dd9a4&sh=667.000000&sm_dt=201902272149077fd4d01a151205278bbc038390e761e30157f0351da1aed5&sw=375.000000&token=wn-GBGsbbwxESEJvSly_PX-Mmk89ZYUn&ts=1562487032370"
-
     hurenData='''nice-sign-v1://ea648ff955ec53cc392f82545c06ab7c:1c51069a471c573a/{"id":"132141","token":"G4eWahyWI3XU4JwHgsX5PITkAsHGK5F0"}'''
     daogouData='''nice-sign-v1://a615a7d33093849f10969da5c4a2a39f:736d5a5e08525ee6/{"id":"87669","token":"G4eWahyWI3XU4JwHgsX5PITkAsHGK5F0"}'''
     heiyeezyData='''nice-sign-v1://c439269145ac860474a0949378d88fa1:b235d822d39e03ed/{"id":"169989","token":"G4eWahyWI3XU4JwHgsX5PITkAsHGK5F0"}'''
@@ -145,36 +165,81 @@ if __name__ == "__main__":
     laomei='''nice-sign-v1://067fabde15d257e5e94a55a0eabd8223:58637c96cc13ac3a/{"id":"99674","token":"wn-GBGsbbwxESEJvSly_PX-Mmk89ZYUn"}'''
     fentianshi='''nice-sign-v1://9cb41e1db0e266d28cc6f4e8e32826c1:8dd9ef08d30b7e9d/{"id":"199600","token":"wn-GBGsbbwxESEJvSly_PX-Mmk89ZYUn"}'''
 
-    n=nice()
-    chajia=200
-    # r=n.getPrice(url, laomei)
-    # n.calculatePrice(r,"美洲限定"chajia)
-    while (True):
-        X=judgeTime("07:20", "23:59")
-        if(X):
-            while(judgeTime("07:20", "23:59")):
-                r=n.getPrice(url,heimanData)
-                n.calculatePrice(r,"黑满天星",1000)
+    url2='''https://115.182.19.49/Sneakerpurchase/priceInfosV1?sign=b579538aeb155166cb6d4e4ded8af04e&token=ujt9rjESVVTNgieTokeuP6We0yEJmsYA&did=77e71f31fc1dec1e7eaebf9f6f822aa3&osn=android&osv=6.0&appv=5.4.3&net=0-0-wifi&longitude=121.45194050763632&latitude=31.18470913241727&ch=main&lp=50.0&geoacc=50.0&geotype=wifi&ver=1&ts=1563765346773&sw=1080&sh=1920&la=zh-CN&seid=022d00753306894490c6b1b9716e20fe&src=&tpid=&pre_module_id=&module_id=&lm=mobile&abroad=no&country=&sm_dt=201907051933014593e25f7ddd86177727b95cc95e1c140171e2bb8719a5d7&n_ssid=%22PingAnNet_BX%22&n_bssid=00:fe:c8:e5:a1:3a&n_dns=43.254.104.76&n_lac=0&n_tac=&n_cid=0&n_mcc=0&n_mnc=0&n_bsss=-1&n_misi=unknown&n_ssn&n_sci=&n_son=&g_x=0.0&g_y=0.0&g_z=0.0&a_x=1.593&a_y=7.853&a_z=4.962&dt=ZTE%20BV0701&dn=ZTE%20BV0701_8507&im=A00000568D4FA1&dvi=753f3f2a559523f9ea6dba752c8c7e0c0335851568e0abb4bbf3ad0d020c0d7c'''
+    sacai='''nice-sign-v1://ecca8b4e94313b201a0aac0ceba0eea9:yyqnxpkswxa1vlad/{"id":"95618"}'''
+    heifen='''nice-sign-v1://0b9ed6a0a7222d54decfe2f01510a03c:gq5z0y9pan7qviel/{"id":"154035"}'''
+    hongou='''nice-sign-v1://510cce818f9d5cd1168628eff79a3d78:v7tdjkmkvlj56os9/{"id":"103024"}'''
 
-                r=n.getPrice(url,daogouData)
-                n.calculatePrice(r,"倒钩",2000)
+    chajia=150
 
-                r=n.getPrice(url,offwhitebeika)
-                n.calculatePrice(r,"北卡蓝",1500)
+    threads = []
+    t1 = threading.Thread(target=bot,args=(url,heimanData,"黑满天星",3000,4))
+    threads.append(t1)
+    t2 = threading.Thread(target=bot,args=(url,daogouData,"倒钩",3000,3))
+    threads.append(t2)
+    t3 = threading.Thread(target=bot,args=(url,offwhitebeika,"北卡蓝",3000,4))
+    threads.append(t3)
+    t4 = threading.Thread(target=bot,args=(url,hurenData,"湖人",600,3))
+    threads.append(t4)
+    t5 = threading.Thread(target=bot,args=(url,heiyeezyData,"黑天使",350,1.5))
+    threads.append(t5)
+    t6 = threading.Thread(target=bot,args=(url,fentianshi,"粉天使",chajia,1.5))
+    threads.append(t6)
+    t7 = threading.Thread(target=bot,args=(url,baiyeezy,"白冰激凌",chajia,3))
+    threads.append(t7)
+    t8 = threading.Thread(target=bot,args=(url,laomei,"美洲限定",chajia,2))
+    threads.append(t8)
+    t9 = threading.Thread(target=bot,args=(url2,sacai,"SACAI",chajia,2))
+    threads.append(t9)
+    t10 = threading.Thread(target=bot,args=(url2,heifen,"黑粉",chajia,2))
+    threads.append(t10)
+    t11 = threading.Thread(target=bot,args=(url2,hongou,"红勾",chajia,2))
+    threads.append(t11)
+    for t in threads:
+         t.setDaemon(True)
+         t.start()
+    t.join()
 
-                r=n.getPrice(url,hurenData)
-                n.calculatePrice(r,"湖人",500)
+    # r=getPrice(url2, hongou)
+    # calculatePrice(r,"红勾",chajia)
+    # bot(url,heimanData,"黑满天星",2000)
 
-                r=n.getPrice(url,heiyeezyData)
-                n.calculatePrice(r,"黑天使",400)
-
-                r=n.getPrice(url, fentianshi)
-                n.calculatePrice(r,"粉天使",chajia)
-
-                r=n.getPrice(url,baiyeezy)
-                n.calculatePrice(r,"白冰激凌",chajia)
-
-                r=n.getPrice(url, laomei)
-                n.calculatePrice(r,"美洲限定",chajia)
-
-                time.sleep(3)
+    # while (True):
+    #     X=judgeTime("07:20", "23:59")
+    #     if(X):
+    #         while(judgeTime("07:20", "23:59")):
+    #             r=getPrice(url,heimanData)
+    #             calculatePrice(r,"黑满天星",3000)
+    #
+    #             r=getPrice(url,daogouData)
+    #             calculatePrice(r,"倒钩",3000)
+    #
+    #             r=getPrice(url,offwhitebeika)
+    #             calculatePrice(r,"北卡蓝",3000)
+    #
+    #             r=getPrice(url,hurenData)
+    #             calculatePrice(r,"湖人",650)
+    #
+    #             r=getPrice(url,heiyeezyData)
+    #             calculatePrice(r,"黑天使",450)
+    #
+    #             r=getPrice(url, fentianshi)
+    #             calculatePrice(r,"粉天使",chajia)
+    #
+    #             r=getPrice(url,baiyeezy)
+    #             calculatePrice(r,"白冰激凌",chajia)
+    #
+    #             r=getPrice(url, laomei)
+    #             calculatePrice(r,"美洲限定",chajia)
+    #
+    #
+    #             r=getPrice(url2, sacai)
+    #             calculatePrice(r,"SACAI",chajia)
+    #
+    #             r=getPrice(url2, heifen)
+    #             calculatePrice(r,"黑粉",chajia)
+    #
+    #             r = getPrice(url2, hongou)
+    #             calculatePrice(r, "红勾", chajia)
+    #
+    #             time.sleep(2)
--- PythonDemo/src/testpython/nice_airtest.py
+++ PythonDemo/src/testpython/nice_airtest.py
@@ -41,7 +41,13 @@ class nice_airtest():
                 else:
                     swipe((150, 800), (150, 0))
                     sleep(0.2)
-                    self.waitFor("contains", name).click()
+                    if self.waitForVisibleOrNot("contains", name, 1):
+                        self.waitFor("contains", name, 1).click()
+                    else:
+                        swipe((150, 800), (150, 0))
+                        sleep(0.2)
+                        self.waitFor("contains", name).click()
+
             else:
                 stop_app("com.nice.main")
                 start_app("com.nice.main")
@@ -53,15 +59,37 @@ class nice_airtest():
                 else:
                     swipe((150, 800), (150, 0))
                     sleep(0.2)
-                    self.waitFor("contains", name).click()
+                    if self.waitForVisibleOrNot("contains", name, 1):
+                        self.waitFor("contains", name, 1).click()
+                    else:
+                        swipe((150, 800), (150, 0))
+                        sleep(0.2)
+                        self.waitFor("contains", name).click()
 
             poco("com.nice.main:id/sdv_cover").click()
             poco("com.nice.main:id/tv_buy").click()
 
             if poco("android.support.v7.widget.RecyclerView").offspring("com.nice.main:id/rdv_desc").sibling("android.widget.RelativeLayout").offspring(text=size).exists():
                 goodsPrice=poco("android.support.v7.widget.RecyclerView").offspring("com.nice.main:id/rdv_desc").sibling("android.widget.RelativeLayout").offspring(text=size).parent().offspring("com.nice.main:id/ll_price").offspring("com.nice.main:id/tv_price").get_text().replace("¥","")
-                if(goodsPrice==price):
+                if(int(goodsPrice)<=int(price)):
                     poco("android.support.v7.widget.RecyclerView").offspring("com.nice.main:id/rdv_desc").sibling("android.widget.RelativeLayout").offspring(text=size).click()
+                    #  购买下单
+                    poco("com.nice.main:id/tv_storage_title").click()
+                    poco("com.nice.main:id/btn_submit").click()
+                    poco("com.nice.main:id/btn_confirm").click()
+                    print("已自动下单" + name + size + "码,价格:" + goodsPrice)
+                    subject = "已自动下单" + name + size + "码,价格:" + goodsPrice + ",请速确认是否购买成功"
+                    sendqqEmail.sendqqEmail(subject, subject)
+                    stop_app("com.nice.main")
+                    start_app("com.nice.main")
+                    poco(text="我").click()
+                    poco("com.nice.main:id/txt_want").click()
+                else:
+                    print("价格不满足预期,返回商品页")
+                    stop_app("com.nice.main")
+                    start_app("com.nice.main")
+                    poco(text="我").click()
+                    poco("com.nice.main:id/txt_want").click()
             else:
                 for i in range(14):
                     swipe((150, 800), (150, 0))
@@ -108,7 +136,7 @@ if __name__ == "__main__":
     # name='''YEEZY BOOST 350 V2 "CREAM WHITE" 白冰淇淋'''
     # name='''NIKE SB x AIR JORDAN 1 联名 2019年版 "LA TO CHICAGO" 湖人 芝加哥 刮刮乐'''
     # name='''TRAVIS SCOTT x AIR JORDAN 1 联名 "CACTUS JACK" 2018年版 反钩 倒钩'''
-    size="47 1/3"
+    size="42"
     b=nice_airtest()
     b.buy(name,size,"998")
     # sendqqEmail.sendqqEmail(name,name)
